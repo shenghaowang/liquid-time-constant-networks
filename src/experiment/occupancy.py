3149a7f0f174cb9f8adcb836f48c05a3ce133f97
root <wangshenghao1993@gmail.com>
2025-02-14 05:13:02 +0000

    Train the LSTM model

--- src/experiment/occupancy.py
+++ src/experiment/occupancy.py
@@ -1,8 +1,10 @@
 import hydra
+import pytorch_lightning as pl
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
 
 from data.occupancy import DataModule
+from model.lstm import LSTM
 
 
 # https://stackoverflow.com/questions/62664386/how-to-pass-a-hydra-config-via-command-line
@@ -15,6 +17,7 @@ def main(cfg: DictConfig):
         test_files=cfg.data.test,
         feature_cols=cfg.data.features,
         target_col=cfg.data.target,
+        seq_len=cfg.data.seq_len,
     )
     dm.setup()
 
@@ -24,6 +27,18 @@ def main(cfg: DictConfig):
 
     logger.debug(f"Features: {features.shape}")
     logger.debug(f"Labels: {labels.shape}")
+
+    model = LSTM(
+        input_dim=len(cfg.data.features), hidden_dim=32, output_dim=cfg.data.seq_len
+    )
+    trainer = pl.Trainer(
+        max_epochs=3,
+        accelerator="auto",
+        devices=2,
+    )
+    trainer.fit(model, dm)
+
+    trainer.test(model, datamodule=dm)
 
 
 if __name__ == "__main__":
